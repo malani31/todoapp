b6b9ba567855d79ff82e70c087cd9ccb137988a1
malani31 <princemalani50@gmail.com>
2026-06-14 12:33:06 +0530

    fix production API url

--- server/index.js
+++ server/index.js
@@ -14,19 +14,37 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 // Connect to MongoDB
-mongoose.connect(`${process.env.MONGO_DB_URL}`, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // writeConcern: {
-    //   w: 'majority',
-    // }
-  })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-      console.error('Failed to connect to MongoDB', error);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
+  } catch (err) {
+    console.log("DB connection error:", err);
+  }
+};
+
+startServer();
+// mongoose.connect(`${process.env.MONGO_DB_URL}`, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//     // writeConcern: {
+//     //   w: 'majority',
+//     // }
+//   })
+//     .then(() => {
+//       console.log('Connected to MongoDB');
+//     })
+//     .catch((error) => {
+//       console.error('Failed to connect to MongoDB', error);
+//     });
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 })
